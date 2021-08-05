0875d675cbb6af4927725b3b1281c4dbc201c93f
Sedina <sat9978l@nyu.edu>
2021-08-05 16:59:06 -0400

    .

--- test/game_test.py
+++ test/game_test.py
@@ -1,13 +1,28 @@
 
 # # TODO: import some code
 
+#getting into test driven development (write test first then update the app)
+
+from app.game import determine_winner
+
+# determine_winner = 
+
+
+# u = "rock"
+# c = "scissors"
+# winner = "USER" # / rock
+
+def test_enlarge():
+    assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
+    assert determine_winner(user_choice="rock", computer_choice="rock") == None
+    assert determine_winner(user_choice="rock", computer_choice="scissors") == "rock"
+    # assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
+    # assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
+    # # assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
+
 # def my_func():
 # def enlarge (i):
 #     return i * 100
 # # TODO: test the code
 
-# from app.my_script import enlarge
 
-# def test_enlarge():
-#     result = enlarge(3)
-#     assert result == 300
